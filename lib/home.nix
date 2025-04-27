2e15c9cbe8476918bfad18943b82a7fefe3cdd24
Jiho Lee <optional.int@kakao.com>
2025-04-27 19:18:51 +0900

    feat: Add home-manager packages

--- lib/home.nix
+++ lib/home.nix
@@ -13,5 +13,73 @@
     nerd-fonts.symbols-only
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
-  ];
+	coreutils-full
+	terraform
+	openssh
+	ansible
+	fira-code
+	zsh
+	zplug
+	neovim
+	neovide
+	tldr
+	gh
+	bat
+	neofetch
+	curl
+	wget
+	thefuck
+	nodejs
+	gcc
+	glibc
+	libiconv
+	fzf
+	gnupg
+	go
+	obsidian
+	kubectl
+	kubectx
+	rustc
+	vscode
+	nerdfonts
+	jetbrains-mono
+	wezterm
+	lazygit
+	k9s
+	git
+	rclone
+	unixtools.nettools
+	awscli2
+	yq-go
+	jq
+	tmux
+	lsof
+	kubernetes-helm
+	zip
+	unzip
+	imgcat
+	python3
+	direnv
+	opentofu
+    zoxide
+	htop
+	slack
+	playonlinux
+	pure-prompt
+	libxcrypt
+	brave
+    kubetail
+  ] ++ (if system == "x86_64-darwin" || system == "aarch64-darwin" then [
+        # macOS-only packages
+        iterm2
+        karabiner-elements 
+      ] else if system == "x86_64-linux" || system == "aarch64-linux" then [
+        # Linux-only packages
+        chromium
+        jetbrains.idea-ultimate
+        kime
+        google-chrome
+        firefox
+        xclip # Clipboard
+      ] else []);
 }
