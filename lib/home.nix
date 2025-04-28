badf9b3a48ba1561b757b38811351fcfb074e303
Jiho Lee <optional.int@kakao.com>
2025-04-28 17:28:39 +0900

    fix: Remove parantheses on home.homeDirectory

--- lib/home.nix
+++ lib/home.nix
@@ -2,15 +2,16 @@
 
 {
   home.username = userConfig.username;
-  home.homeDirectory = (if userConfig.system == "x86_64-darwin"
+  home.homeDirectory = if userConfig.system == "x86_64-darwin"
   || userConfig.system == "aarch64-darwin" then
     "/Users/${userConfig.username}"
-  else if userConfig.system == "x86_64-linux" || userConfig.system
-  == "aarch64-linux" then
+  else if userConfig.system == "x86_64-linux"
+  || userConfig.system  == "aarch64-linux" then
     "/home/${userConfig.username}"
     # Assuming windows based system
   else
-    "C:\\Users\\${userConfig.username}");
+    "C:\\Users\\${userConfig.username}";
+
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = true; # 폰트 설정 활성화
