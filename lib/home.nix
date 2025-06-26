0e1f387f83bcab4e0ee7a15bb43606faf5f070e8
Jiho Lee <optional.int@kakao.com>
2025-06-26 15:08:30 +0900

    feat: Install wireshark

--- lib/home.nix
+++ lib/home.nix
@@ -1,21 +1,26 @@
-{ config, pkgs, userConfig, ... }:
+{
+  config,
+  pkgs,
+  userConfig,
+  ...
+}:
 
 {
   home.username = userConfig.username;
-  home.homeDirectory = if userConfig.system == "x86_64-darwin"
-  || userConfig.system == "aarch64-darwin" then
-    "/Users/${userConfig.username}"
-  else if userConfig.system == "x86_64-linux" || userConfig.system
-  == "aarch64-linux" then
-    "/home/${userConfig.username}"
+  home.homeDirectory =
+    if userConfig.system == "x86_64-darwin" || userConfig.system == "aarch64-darwin" then
+      "/Users/${userConfig.username}"
+    else if userConfig.system == "x86_64-linux" || userConfig.system == "aarch64-linux" then
+      "/home/${userConfig.username}"
     # Assuming windows based system
-  else
-    "C:\\Users\\${userConfig.username}";
+    else
+      "C:\\Users\\${userConfig.username}";
 
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = true; # 폰트 설정 활성화
-  home.packages = with pkgs;
+  home.packages =
+    with pkgs;
     [
       nerd-fonts.symbols-only
       nerd-fonts.fira-code
@@ -81,20 +86,28 @@
       brave
       kubetail
       windsurf
-    ] ++ (if system == "x86_64-darwin" || system == "aarch64-darwin" then [
-      # macOS-only packages
-      iterm2
-      karabiner-elements
-    ] else if system == "x86_64-linux" || system == "aarch64-linux" then [
-      # Linux-only packages
-      chromium
-      jetbrains.idea-ultimate
-      kime
-      google-chrome
-      firefox
-      xclip # Clipboard
-      glibc
-      playonlinux
-    ] else
-      [ ]);
+      wireshark
+    ]
+    ++ (
+      if system == "x86_64-darwin" || system == "aarch64-darwin" then
+        [
+          # macOS-only packages
+          iterm2
+          karabiner-elements
+        ]
+      else if system == "x86_64-linux" || system == "aarch64-linux" then
+        [
+          # Linux-only packages
+          chromium
+          jetbrains.idea-ultimate
+          kime
+          google-chrome
+          firefox
+          xclip # Clipboard
+          glibc
+          playonlinux
+        ]
+      else
+        [ ]
+    );
 }
