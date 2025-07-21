0e7d8d3425dd0f4b53bae54b7711760aed216ce9
Jiho Lee <optional.int@kakao.com>
2025-07-21 11:33:12 +0900

    feat: Update stateVersion from 24.11 to 25.05

--- lib/home.nix
+++ lib/home.nix
@@ -16,7 +16,7 @@
     else
       "C:\\Users\\${userConfig.username}";
 
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.05";
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = true; # 폰트 설정 활성화
   home.packages =
